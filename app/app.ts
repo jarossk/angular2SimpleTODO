59b04e119122380c8a0a393e98ce7d4386537e92
Jarek <klata.jarek@gmail.com>
2016-02-14 17:39:00 +0000

    App component & todoList

---
+++ app/app.ts
@@ -0,0 +1,17 @@
+import {Component, bootstrap} from 'angular2/angular2';
+import {TodoList} from './app/todoList.ts';
+
+@Component{(
+  selector: 'app',
+  directives: [TodoList],
+  template: 
+    <h1>Todo App</h1>
+    <todo-list></todo-list>
+)}
+class AppComponent {
+  constructor() {
+    
+  }
+}
+bootstrap(AppComponent);
+
