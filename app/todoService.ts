fb20cbf0851ad956cda555edd8496cf5836fdfe4
Jarek <klata.jarek@gmail.com>
2016-02-14 18:30:52 +0000

    Created todoService with 4 methods

---
+++ app/todoService.ts
@@ -0,0 +1,43 @@
+export class TodoService {
+  
+  private _todos;
+  constructor() {
+    this._todos = todos;
+  }
+  getTodos() {
+    return this._todos;
+  }
+  completed(todo) {
+    todo._isCompleted = todo.isCompleted ? false : true;
+  }
+  delete(todo) {
+    var index = this._todos.indexOf(todo);
+    this._todos.splice(index, 1);
+  }
+  add(content: string) {
+    var todo = {
+      content: content,
+      createdAt: new Date(),
+      isCompleted: false
+    }
+    this._todos.push(todo);
+  }
+}
+
+var todos = [
+  {
+    content: 'Learn Angular 2',
+    createdAt: new Date(),
+    isCompleted: false
+  },
+  {
+    content: 'Learn TypeScript',
+    createdAt: new Date(),
+    isCompleted: false
+  },
+  {
+    content: 'Learn Node.js',
+    createdAt: new Date(),
+    isCompleted: true
+  }
+]
